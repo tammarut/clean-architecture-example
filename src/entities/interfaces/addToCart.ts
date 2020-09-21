e6301b260d2194662c1ff6fbe615fd6ef6942a6b
tammarut <tammarut_nua@true-e-logistics.com>
2020-09-21 22:59:54 +0700

    🏗  Policy layer: Entity

---
+++ src/entities/interfaces/addToCart.ts
@@ -0,0 +1,7 @@
+import AddedToCart from '../types/AddedToCart'
+import Product from '../types/Product'
+
+export default interface IAddToCart{
+  add(item: Product): Promise<AddedToCart>
+}
+
---
+++ src/entities/types/Product.ts
@@ -0,0 +1,3 @@
+type Product = {}
+
+export default Product
